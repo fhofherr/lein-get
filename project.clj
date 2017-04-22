f1723d2f06083cd1b558a7a138ede069b420a7d4
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2017-04-22 11:38:05 +0200

    Initial project setup

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fhofherr/lein-get "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/fhofherr/lein-get"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/get.clj
@@ -0,0 +1,6 @@
+(ns leiningen.get)
+
+(defn get
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
